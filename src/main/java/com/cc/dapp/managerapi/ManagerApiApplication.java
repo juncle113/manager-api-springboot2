ba2add01024c50af68c8481552b0f2817aeebe52
flh_sunli <sunli20140406@dingtalk.com>
2018-11-23 16:16:11 +0800

    添加配置文件

--- src/main/java/com/cc/dapp/managerapi/ManagerApiApplication.java
+++ src/main/java/com/cc/dapp/managerapi/ManagerApiApplication.java
@@ -2,8 +2,16 @@ package com.cc.dapp.managerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableSwagger2
+//@EnableScheduling
+//@EnableCaching
 public class ManagerApiApplication {
 
     public static void main(String[] args) {
