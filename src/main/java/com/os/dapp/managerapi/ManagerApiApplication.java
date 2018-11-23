5877975a7ee71321943d465c5a96815b0dd9f816
flh_sunli <sunli20140406@dingtalk.com>
2018-11-23 09:32:38 +0800

    项目初始化

---
+++ src/main/java/com/os/dapp/managerapi/ManagerApiApplication.java
@@ -0,0 +1,12 @@
+package com.os.dapp.managerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/os/dapp/managerapi/ManagerApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.os.dapp.managerapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManagerApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
